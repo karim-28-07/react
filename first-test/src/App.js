9437b48b01f7f865fb545939aa697640646056d2
karim <karim.mezouar@gmail.com>
2021-04-07 14:46:41 +0200

    react creation

---
+++ first-test/src/App.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+
+
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div >
+        <h1>Karim <button>x</button></h1>
+
+      </div>
+    );
+  }
+}
+
+export default App;
