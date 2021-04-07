c680819495572aa427f0c5b1142dc35dbb8a9e7f
karim <karim.mezouar@gmail.com>
2021-04-07 15:43:09 +0200

    setup and afichage

--- chapitre_01/elsy/src/App.js
+++ chapitre_01/elsy/src/App.js
@@ -1,12 +1,29 @@
-
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1> Salut</h1>
-    </div>
-  );
+const tempMin = -20
+const tempMax = 40
+const heartMin = 80
+const heartMax = 180
+const stepsMin = 0
+const stepsMAx = 50000
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className = "container-fluid">
+        
+        <div className = "row">
+
+          <p>Heart : {heartMin} </p>
+          <p>Temperature : {tempMin} </p>
+          <p>Heart : {stepsMin} </p>
+
+        </div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
