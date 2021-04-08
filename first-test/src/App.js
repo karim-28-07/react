78bdac0f2166daabc3bb14140baab35e7c82783c
karim <karim.mezouar@gmail.com>
2021-04-08 17:31:36 +0200

    first-test

--- first-test/src/App.js
+++ first-test/src/App.js
@@ -9,9 +9,9 @@ class App extends React.Component {
   render() {
     return (
       <div >
-        <NavBar />
+        
         <h1> Salut Karim, Comment vas tu ? <button>x</button></h1>
-        <Button />
+        
 
       </div>
     );
--- first-test/src/index.js
+++ first-test/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import  './Components';
+
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Component />
   </React.StrictMode>,
   document.getElementById('root')
 );
