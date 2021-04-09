1a0f9710b6305886ac0337c3c88d060e73d72372
karim <karim.mezouar@gmail.com>
2021-04-09 17:26:39 +0200

    elsy part 2 + flags_of_the_world

--- chapitre_01/elsy/src/App.js
+++ chapitre_01/elsy/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Box from './components/Box';
 import './styles/global.css'
 
+
 const tempMin = -20
 const tempMax = 40
 const heartMin = 80
@@ -11,30 +12,52 @@ const stepsMin = 0
 const stepsMAx = 50000
 
 class App extends React.Component {
-  
+
+  constructor() {
+    super()
+
+    this.state = {
+      water: 0,
+      heart: 120,
+      temperature: -10,
+      steps: 3000
+
+    }
+
+
+
+  }
+
+
+
   render() {
     return (
-      <div className = "container-fluid">
-        
-        <div className = "row">
-          {/* Water */}
-          <Box icon = "local_drink" color = "#3A85FF" value = {1.5} unit = "L"></Box>
-          {/* Steps */}
-          <Box icon = "directions_walk" color = "black" value = {3000} unit = "steps"  ></Box>
-          {/* Heart */}
-          <Box icon = "favorite" color = "red" value = {120} unit = "bpm"></Box>
-          {/* Temperature */}
-          <Box icon = "wb_sunny" color = "yellow" value = {-10} unit = "°C"></Box>
-
-          <p>Heart : {heartMin} </p>
-          <p>Temperature : {tempMin} </p>
-          <p>Heart : {stepsMin} </p>
-        </div>
-
-
-
+      <div className="container-fluid">
 
         
+            <div className="row">
+
+              {/* Water */}
+              <Box icon="local_drink" color="#3A85FF" value={1.5} unit="L"></Box>
+              {/* Steps */}
+              <Box icon="directions_walk" color="black" value={3000} unit="steps"  ></Box>
+              {/* Heart */}
+              <Box icon="favorite" color="red" value={120} unit="bpm"></Box>
+              {/* Temperature */}
+              <Box icon="wb_sunny" color="yellow" value={-10} unit="°C"></Box>
+
+              <p>Heart : {heartMin} </p>
+              <p>Temperature : {tempMin} </p>
+              <p>Heart : {stepsMin} </p>
+
+
+              
+            </div>
+
+
+
+
+
       </div>
     );
   }
---
+++ flags_of_the_world/src/pages/About.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+
+        <div>
+            <Navigation />
+            <h1>À propos</h1>
+            <br />
+            <p> Lorem Ipsum est simplement du faux texte
+            employé dans la composition et la mise en
+            page avant impression. Le Lorem Ipsum est
+            le faux texte standard de l'imprimerie depuis
+            les années 1500, quand un imprimeur anonyme
+            assembla ensemble des morceaux de texte pour
+            réaliser un livre spécimen de polices de texte.
+            'a pas fait que survivre cinq siècles, mais
+            s'est aussi adapté à la bureautique informatique,
+            ns que son contenu n'en soit modifié.
+            Il a été popularisé dans les années 1960
+            grâce à la vente de feuilles Letraset contenant
+            des passages du Lorem Ipsum, et, plus récemment,
+            par son inclusion dans des applications de mise
+                en page de texte, comme Aldus PageMaker.</p>
+            <br />
+            <p>Lorem Ipsum est simplement du faux texte employé
+            dans la composition et la mise en page avant
+            impression. Le Lorem Ipsum est le faux texte
+            standard de l'imprimerie depuis les années 1500,
+            nd un imprimeur anonyme assembla ensemble des
+            morceaux de texte pour réaliser un livre spécimen
+                      olices de texte. .</p>
+
+        </div>
+
+    );
+}
+
+export default About;
+
