ccdd3d7773aaff577f6d887bcde647b4f7b9f4de
karim <karim.mezouar@gmail.com>
2021-04-16 12:29:26 +0200

    bakery

--- chapitre_04/bakery/src/App.js
+++ chapitre_04/bakery/src/App.js
@@ -9,13 +9,13 @@ import Button from "./Components/Button"
 
 class App extends React.Component {
 
-  constructor(){
+  constructor() {
     super()
 
     this.state = {
-      activeTab : "add",
-      items : [],
-      
+      activeTab: "add",
+      items: [],
+
     }
 
     this.selectAdd = this.selectAdd.bind(this)
@@ -23,43 +23,111 @@ class App extends React.Component {
     this.selectPay = this.selectPay.bind(this)
   }
 
+  addItem(name,price){
+    const obj = { name : 'xx', price : 'xx'}
+    this.setState ({ 
 
-  selectAdd(){
-    this.setState({activeTab : 'add'});
+     items : [...this.state.items, obj]
+      
+    })
+    console.log(this.state.items);
+  }
+
+
+  selectAdd() {
+    this.setState({ activeTab: 'add' });
     
   }
-  
 
 
-  selectList(){
-    this.setState({activeTab : 'List'});
+
+  selectList() {
+    this.setState({ activeTab: 'List' });
   }
 
-  selectPay(){
-    this.setState({activeTab : 'Pay'});
+  selectPay() {
+    this.setState({ activeTab: 'Pay' });
   }
 
-  
 
-  render (){
+  renderHead() {
 
-    return(
+    return (
       <div>
         <h1>Bakery</h1>
-        {/* <Add/>
-        <Pay/>
-        <List/> */}
-        <button type="button" class="btn btn-outline-primary" 
-        isSelected = {this.state.activeTab}>Add</button> 
         <button type="button" class="btn btn-outline-primary"
-         isSelected = {this.state.activeTab}>List</button> 
-        <button type="button" class="btn btn-outline-primary" 
-        isSelected = {this.state.activeTab}>Pay</button> 
-      </div> 
-    )
+          isSelected={this.state.activeTab} onClick={this.selectAdd} >Add</button>
+        <button type="button" class="btn btn-outline-primary"
+          isSelected={this.state.activeTab} onClick={this.selectList} >List</button>
+        <button type="button" class="btn btn-outline-primary"
+          isSelected={this.state.activeTab} onClick={this.selectPay} >Pay</button>
 
+      </div>
+    )
   }
+
+  renderContent() {
+    if (this.state.activeTab == 'add') {
+      return (
+        <section>
+          <h2><Add /></h2>
+        </section>
+      );
+    } else if (this.state.activeTab == 'List')
+      return (
+        <section>
+          <h2><List /></h2>
+        </section>
+      );
+
+    else if (this.state.activeTab == 'Pay')
+    return (
+      <section>
+        <h2><Pay /></h2>
+      </section>
+    )
+  }
+
+
+
+
+
+
+render() {
+
+  return (
+    <div>
+
+      <Button />
+      <Button />
+      <Button />
+      {this.renderHead()}
+      {this.renderContent()}
+
+    </div>
+  )
+
+}
 
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- chapitre_04/bakery/src/views/Add.jsx
+++ chapitre_04/bakery/src/views/Add.jsx
@@ -2,6 +2,29 @@ import React from 'react'
 
 class Add extends React.Component {
 
+    constructor(){
+        super()
+
+        this.state ={
+            productName : '',
+            price: 1
+        }
+        this.updateProductName = this.updateProductName.bind(this)
+        this.updatePrice = this.updatePrice.bind(this)
+    }
+
+    updateProductName(event){
+        this.setState ({
+            productName : event.target.value
+        })
+    }
+
+    updatePrice(event){
+        this.setState ({
+            price : event.target.value
+        })
+    }
+
 
     render(){
 
@@ -9,6 +32,9 @@ class Add extends React.Component {
 
             <div>
                 Add
+                <input type ='text' onChange = {this.updateProductName}/>
+                <input type = 'range' min = {1} max= {10} onChange ={this.updatePrice}/>
+                <button type = 'Add' onClick = {this.props.price}/>
             </div>
         )
     }
