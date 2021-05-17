3f81d0a2ef72cc2c8fbb2c43b6f08e93382e1279
karim <karim.mezouar@gmail.com>
2021-05-17 17:38:33 +0200

    component Favorites

--- chapitre_06/moovice/src/views/Favorites.jsx
+++ chapitre_06/moovice/src/views/Favorites.jsx
@@ -3,34 +3,55 @@ import React, { Component } from 'react'
 export class Favorites extends Component {
 
     state = {
-        movies : [],
-        favIDs : this.getStorage()
+        movies: [],
+        favIDs: this.getStorage()
     }
 
-    getStorage(){
+    getStorage() {
 
-        const favorites = localStorage.getItem("favorites")
-
+        const favorites = JSON.parse(localStorage.getItem("favorites"))
+        console.log("Favorites tyepof favorites", typeof(favorites))
         return favorites
     }
 
-    getMovie(id){
+    
+    getMovie(id) {
 
-        fetch("http://api.themoviedb.org/3/movie/{ID}?api_key={API_KEY}")
-        .then(response => response.json())
+        const apiKey = "e441f8a3a151d588a4932d2c5d310769"
+
+        fetch(`http://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`)
+            .then(response => response.json())
             .then(data => {
+                console.log("Favorites data", data)
+
+                this.setState({
+                    movies: [...this.state.movies, data]
+                })
+
 
 
             })
 
-        }
+    }
+
+    componentDidMount() {
+        
+            this.state.favIDs.map(item => {
+                
+                return this.getMovie(item)
+            })
+        
+    }
 
     render() {
 
-        console.log(" Favorites this.getStorage ", this.getStorage());
+        console.log("Favorites this.state.movies", this.state.movies)
+
         return (
+
             <div>
                 <h1>Favorites</h1>
+                {/* {this.state.favIDs} */}
             </div>
         )
     }
--- chapitre_06/moovice/src/views/Weekly.jsx
+++ chapitre_06/moovice/src/views/Weekly.jsx
@@ -1,13 +1,77 @@
 import React, { Component } from 'react'
+import Card from '../components/Card'
+
+var moment = require("moment");
+
 
 export class Weekly extends Component {
+
+    state = {
+        movies: []
+    }
+
+    formatDateToday(dateString){
+
+        console.log("Weekly datetoday",moment(dateString).format("YYYY-MM-DD"));
+        const dateToday = moment(dateString).format("YYYY-MM-DD")
+        return dateToday
+    }
+    
+    // formatDateToday()
+    
+    formatDateLastWeek(dateString){
+        console.log("Weekly datelastweek", moment(dateString).subtract(6, 'days').format("YYYY-MM-DD"));
+        const lastWeek =  moment(dateString).subtract(6, 'days').format("YYYY-MM-DD")
+        return lastWeek
+    }
+    
+    
+    // formatDateLastWeek()
+    
+    
+
+    componentDidMount() {
+
+        const apiKey = "e441f8a3a151d588a4932d2c5d310769"
+
+        fetch("http://api.themoviedb.org/3/discover/movie?primary_release_date.gte="+ this.formatDateLastWeek()+"&primary_release_date.lte="+this.formatDateToday()+"&api_key=${apiKey}")
+            .then(response => response.json())
+            .then(data => {
+
+                console.log("Popular data ", data);
+                console.log("Popular data ", data.results);
+
+                this.setState({
+                    movies: data.results
+                })
+
+            })
+    }
+
+    
     render() {
+        console.log("Popular state movies ", this.state.movies);
         return (
             <div>
-                <h1>Weekly</h1>
+                <h1>Popular</h1>
+              
+
+                {/* {
+                    this.state.movies.map(elem => {
+                        return <Card {...elem} />
+                    })
+                } */}
             </div>
         )
+
     }
+
 }
 
+
+
+
+
+
 export default Weekly
+
