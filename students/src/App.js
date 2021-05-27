444c35ea57593030c5981d6c775b9133f500019e
karim <karim.mezouar@gmail.com>
2021-05-27 16:22:00 +0200

    Students

---
+++ students/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {BrowserRouter, Switch} from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <h1>Students</h1>
+    </BrowserRouter>
+  );
+}
+
+export default App;
